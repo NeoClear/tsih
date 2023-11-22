c26497d73f7d182bdb060d218472482c063a78ea
NeoClear <neoclear@outlook.com>
2023-11-21 19:17:18 -0800

    [tsih/neoclear] Polished linting script

--- script/lint.sh
+++ script/lint.sh
@@ -7,6 +7,14 @@ if [[ $? -ne 0 ]]; then
 	exit 1
 fi
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	find $ROOT -name "*.h" -or -name "*.cc" -or -name "*.sh" -or -name "BUILD" | xargs sed -i -e '$a\'
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	for file in $(find $ROOT -name "*.h" -or -name "*.cc" -or -name "*.sh" -or -name "BUILD"); do
+		echo >>$file
+	done
+fi
+
 clang-format -i -style=file $ROOT/**/*.h $ROOT/**/*.cc
 buildifier $ROOT/**/BUILD $ROOT/WORKSPACE
 shfmt -l -w $ROOT/**/*.sh
