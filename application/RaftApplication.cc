b2e3ba9859fcab19680f8183d236d6c8e5371af7
NeoClear <neoclear@outlook.com>
2023-12-18 19:50:18 -0800

    [tsih/neoclear] Fixed API bugs & removed useless code

--- application/RaftApplication.cc
+++ application/RaftApplication.cc
@@ -4,52 +4,22 @@
 
 namespace application {
 
-// Status
-// RaftServiceImpl::AppendEntriesRequest(ServerContext* context,
-//                                       const AppendEntriesArgument* request,
-//                                       google::protobuf::Empty* reply) {
-//   const auto [currentTerm, success] = raft_state_.handleAppendEntries(
-//       request->term(), request->leaderid(), request->prevlogindex(),
-//       request->prevlogterm(), request->entries(), request->leadercommit());
+ServerUnaryReactor*
+RaftServiceImpl::AppendEntries(CallbackServerContext* context,
+                               const AppendEntriesArgument* request,
+                               AppendEntriesResult* reply) {
+  const auto [currentTerm, success] = raft_state_.handleAppendEntries(
+      request->term(), request->leaderid(), request->prevlogindex(),
+      request->prevlogterm(), request->entries(), request->leadercommit());
 
-//   return Status::OK;
-// }
+  reply->set_term(currentTerm);
+  reply->set_success(success);
 
-// Status RaftServiceImpl::AppendEntriesReply(ServerContext* context,
-//                                            const AppendEntriesResult*
-//                                            request, google::protobuf::Empty*
-//                                            reply) {
-//   return Status::OK;
-// }
+  ServerUnaryReactor* reactor = context->DefaultReactor();
+  reactor->Finish(Status::OK);
 
-/**
- * @brief Request vote to the current node
- *
- * @param context Misc
- * @param request Request containing vote request information
- * @param reply Vote result
- * @return Status
- */
-// Status RaftServiceImpl::RequestVoteRequest(ServerContext* context,
-//                                            const RequestVoteArgument*
-//                                            request, google::protobuf::Empty*
-//                                            reply) {
-//   const auto [currentTerm, result] = raft_state_.handleVoteRequest(
-//       request->term(), request->candidateid(), request->lastlogindex(),
-//       request->lastlogterm());
-
-//   stub::ElectionStub::replyTo(request->candidateid(), currentTerm, result);
-
-//   return Status::OK;
-// }
-
-// Status RaftServiceImpl::RequestVoteReply(ServerContext* context,
-//                                          const RequestVoteResult* request,
-//                                          google::protobuf::Empty* reply) {
-//   raft_state_.handleVoteReply(request->term(), request->votegranted());
-
-//   return Status::OK;
-// }
+  return reactor;
+}
 
 ServerUnaryReactor*
 RaftServiceImpl::RequestVote(CallbackServerContext* context,
@@ -74,28 +44,13 @@ RaftServiceImpl::RequestVote(CallbackServerContext* context,
 ServerUnaryReactor* RaftServiceImpl::Ping(CallbackServerContext* context,
                                           const PingMessage* request,
                                           google::protobuf::Empty* reply) {
-  raft_state_.handlePingMsg(request->server_identity().server_type(),
-                            request->server_identity().server_index());
+  raft_state_.handlePing(request->server_identity().server_type(),
+                         request->server_identity().server_index());
 
   ServerUnaryReactor* reactor = context->DefaultReactor();
   reactor->Finish(Status::OK);
 
   return reactor;
 }
-
-// Status RaftServiceImpl::Ping(ServerContext* context, const PingMessage*
-// request,
-//                              google::protobuf::Empty* reply) {
-//   utility::logCrit("Received ping from leader");
-//   raft_state_.handlePingMsg(request->server_identity().server_type(),
-//                             request->server_identity().server_index());
-//   return Status::OK;
-// }
-
-// Status RaftServiceImpl::AddTask(ServerContext* context, const Task* request,
-//                                 google::protobuf::Empty* reply) {
-//   raft_state_.handleAddTask(request->value());
-//   return Status::OK;
-// }
 
 } // namespace application
